7732b1821f4063337bc310eb34edd80f360579c5
na9755-bit <na9755@srmist.edu.in>
2026-02-24 10:04:25 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindroneChecker/src/PalindromeCheckerApp.java
+++ PalindroneChecker/src/PalindromeCheckerApp.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
+        System.out.println();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        String text = scanner.nextLine();
+
+        String reversed = new StringBuilder(text).reverse().toString();
+        boolean isPalindrome = text.equalsIgnoreCase(reversed);
+
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
     }
 }
